33d565bb43adf2500f8eb0b33112e2bbeadbf193
Bella Lau <lau.bella@gmail.com>
2016-07-29 09:58:47 -0700

    game create functionality with update to users table

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,14 @@
 class GamesController < ApplicationController
 	def create
+		win = User.find_by(id: params[:game][:winner_id])
+		score_current = win.wins
+		score_new = score_current + 1
+		User.update(win.id, {wins: score_new})
+
+		loss = User.find_by(id: params[:game][:loser_id])
+		score_current = loss.losses
+		score_new = score_current + 1
+		User.update(loss.id, {losses: score_new})
 		
 		game = Game.new(game_params)
 		flash[:errors] = game.errors.full_messages unless game.save
